6bcf13d484beed30df35adec9f67a975e038ee4a
Joe Kutner <jpkutner@gmail.com>
2016-08-12 09:22:37 -0500

    Added db code

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,21 +1,47 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @Controller
 @SpringBootApplication
+@RequestMapping("/")
 public class DemoApplication {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello from Spring Boot with Dev-Tools!";
-    }
+  private RecordRepository repository;
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+  @Autowired
+  public DemoApplication(RecordRepository repository) {
+    this.repository = repository;
+  }
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String home(ModelMap model) {
+    List<Record> records = repository.findAll();
+    model.addAttribute("records", records);
+    model.addAttribute("insertRecord", new Record());
+    return "home";
+  }
+
+  @RequestMapping(method = RequestMethod.POST)
+  public String insertData(ModelMap model,
+                           @ModelAttribute("insertRecord") @Valid Record record,
+                           BindingResult result) {
+    if (!result.hasErrors()) {
+      repository.save(record);
     }
+    return home(model);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 }
